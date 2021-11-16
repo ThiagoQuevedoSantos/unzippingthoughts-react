15d6318f9b21913ed9aedce209b393e885181388
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-11-16 14:44:53 -0300

    call to action btn added on main

--- pages/components/main/main.jsx
+++ pages/components/main/main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SectionOne from './section1/section_one';
 import SectionTwo from './section2/section_two';
 import SectionThree from './section3/section_three';
+import CallToActionBtn from '@/elements/call-to-action-btn';
 
 class Main extends React.Component {
     render() { 
@@ -9,6 +10,8 @@ class Main extends React.Component {
             <>
             <SectionOne/>
             {/* <SectionTwo/> */}
+            <CallToActionBtn text="Contate-nos" position="center" btnStyle="style1"/>
+            {/* <SectionThree/> */}
             </>
         );
     }
