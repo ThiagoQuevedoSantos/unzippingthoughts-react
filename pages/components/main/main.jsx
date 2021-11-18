3a3f81859ef0495a68ec979a120ecbdb831fac88
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-11-18 09:59:29 -0300

    spacing sections from parent container

--- pages/components/main/main.jsx
+++ pages/components/main/main.jsx
@@ -5,18 +5,19 @@ import SectionThree from './section3/section_three';
 import SectionFour from './section4/section_four';
 import CallToAction from '../elements/call-to-action-btn';
 import Contact from './contact/contact';
+import style from "&/home/main/main.module.css";
 
 class Main extends React.Component {
     render() { 
         return (
-            <>
+            <div id={style.main_container} className="flex_c">
             <SectionOne/>
             <SectionTwo/>
             <CallToAction text="Contate-nos" color="primary" variant="outlined" position="center"/>
             <SectionThree/>
             <SectionFour/>
             <Contact/>
-            </>
+            </div>
         );
     }
 }
