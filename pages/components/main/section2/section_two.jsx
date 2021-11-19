e841ea3f835e7f864db4b68190cb07fa95fa7fac
Thiago Quevedo <thiagoquevedodossantos@gmail.com>
2021-11-19 02:06:20 -0300

    adapting section 1 and 2 to mobile

--- pages/components/main/section2/section_two.jsx
+++ pages/components/main/section2/section_two.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import style from '&/home/main/section2/section_two.module.css';
+import style from '&/home/main/section2/Section_two.module.css';
 import DivLeft from './S2divLeft/S2divLeft';
 import DivRight from './S2divRight/S2divRight';
 
